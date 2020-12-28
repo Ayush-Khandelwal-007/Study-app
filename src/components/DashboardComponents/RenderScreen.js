f9f2b98a7462860679bc65c412ed77152cc4f2f8
Ayush <ayushkhandelwal3217@gmail.com>
2020-12-28 16:49:13 +0530

    Added options for plot

--- src/components/DashboardComponents/RenderScreen.js
+++ src/components/DashboardComponents/RenderScreen.js
@@ -1,77 +1,179 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Chart } from 'react-charts'
+import DropDown from './DropDown'
 
 function RenderScreen() {
-  const [data1, setData1] = useState([])
-  useEffect(() => {
-    const callData = () => {
-      axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
-        .then(response => {
-          console.log(response.data.bpi);
-          var result = [];
-          var result1 = [];
+  const [data1, setData1] = useState(null)
+  const [data2, setData2] = useState(null)
+  const [data3, setData3] = useState(null)
+  const [graph1, setGraph1] = useState(1)
+  const [graph2, setGraph2] = useState(2)
+  const [data, setData] = useState(null)
 
-          for (var i in response.data.bpi) {
-            var z = i.split("-");
-            var newDate = new Date(z[0], z[1] - 1, z[2]);
-            // console.log(newDate.getTime());
-            result.push({ primary: newDate, secondary: response.data.bpi[i] });
-            result1.push({ primary: newDate, secondary: response.data.bpi[i] + (Math.random() * 600) - 300 });
-          }
-          setData1([{ label: 'BitCoin', data: result }, { label: 'Random Graph', data: result1 }]);
-        })
+  const dropdownOptions = ['Id1', 'Id2', 'Id3']
+
+  useEffect(() => {
+    const fetchData = () => {
+      const options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coin/1/history/7d',
+        headers: {
+          'x-rapidapi-key': 'b3bdb530acmsha777171e97dd90ep199712jsn1c204b6d3108',
+          'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        // console.log(response);
+        const data = response.data.data.history;
+        var result1 = [];
+        for (var i in data) {
+          var newDate = new Date(data[i].timestamp);
+          result1.push({ primary: newDate, secondary: parseInt(data[i].price) });
+        }
+        // console.log(result1);
+        setData1({ label: 'Id1', data: result1 });
+
+
+      }).catch(function (error) {
+        console.error(error);
+      });
     }
-    callData();
-    var timer=setInterval(callData, 10000);
+    fetchData()
+    const timer = setTimeout(fetchData, 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+  useEffect(() => {
+    const fetchData = () => {
+      const options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coin/2/history/7d',
+        headers: {
+          'x-rapidapi-key': 'b3bdb530acmsha777171e97dd90ep199712jsn1c204b6d3108',
+          'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        // console.log(response);
+        const data = response.data.data.history;
+        var result2 = [];
+        for (var i in data) {
+          var newDate = new Date(data[i].timestamp);
+          result2.push({ primary: newDate, secondary: parseInt(data[i].price) });
+        }
+        // console.log(result2);
+        setData2({ label: 'Id2', data: result2 });
+
+
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+    fetchData()
+    const timer = setTimeout(fetchData, 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+  useEffect(() => {
+    const fetchData = () => {
+      const options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coin/5/history/7d',
+        headers: {
+          'x-rapidapi-key': 'b3bdb530acmsha777171e97dd90ep199712jsn1c204b6d3108',
+          'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        // console.log(response);
+        const data = response.data.data.history;
+        var result3 = [];
+        for (var i in data) {
+          var newDate = new Date(data[i].timestamp);
+          result3.push({ primary: newDate, secondary: parseInt(data[i].price) });
+        }
+        // console.log(result3);
+        setData3({ label: 'Id3', data: result3 });
+
+
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+    fetchData()
+    const timer = setTimeout(fetchData, 1000);
     return () => {
       clearInterval(timer);
     }
   }, [])
 
+  useEffect(() => {
+    if (graph1 === 1) {
+      switch (graph2) {
+        case 1:
+          if (data1 ) setData([data1, data1]);
+          break;
+        case 2:
+          if (data1 && data2) setData([data1, data2]);
+          break;
+        case 3:
+          if (data1 && data3) setData([data1, data3]);
+          break;
 
+        default:
+          setData(null)
+          console.log("data", data)
+          break;
+      }
+    }
+    else if (graph1===2){
 
-  // useEffect(() => {
-  //   axios.get('https://rest.coinapi.io/v1/exchangerate/BTC?apikey=E952333A-CA2F-46B2-8E23-DB5BE021C4BD')
-  //     .then(response => {
-  //       console.log(response);
-  //       var result1 = [];
-  //       // response.data.rates.length
-  //       for (var i = 0; i < 1000; i++) {
+      switch (graph2) {
+        case 1:
+          if (data2 && data1) setData([data2, data1]);
+          break;
+        case 2:
+          if (data2 ) setData([data2, data2]);
+          break;
+        case 3:
+          if (data2 && data3) setData([data2, data3]);
+          break;
 
-  //         // if(i%100===0)console.log(i)
-  //         var date = response?.data?.rates[i].time
+        default:
+          setData(null)
+          console.log("data", data)
+          break;
+      }
+    }
+    else if (graph1===3){
+      switch (graph2) {
+        case 1:
+          if (data3 && data1) setData([data3, data1]);
+          break;
+        case 2:
+          if (data3 && data2) setData([data3, data2]);
+          break;
+        case 3:
+          if (data3) setData([data3, data3]);
+          break;
 
-  //         try {
-  //           var newDate = new Date(date);
-  //           result1.push({ primary: newDate, secondary: response?.data?.rates[i].rate });
-  //         }
-  //         catch (err) {
-  //           console.log(err);
-  //         }
+        default:
+          setData(null)
+          console.log("data", data)
+          break;
+      }
+    }
 
-  //         // console.log(result1);
-
-  //       }
-  //       result1.sort(function (a, b) {
-  //         // Turn your strings into dates, and then subtract them
-  //         // to get a value that is either negative, positive, or zero.
-  //         return new Date(b.primary) - new Date(a.primary);
-  //       });
-
-  //       try {
-  //         setData1([{ label: 'BitCoin', data: result1 }]);
-  //         console.log(result1);
-  //       }
-  //       catch (err) {
-  //         console.log(err);
-  //       }
-
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     })
-  // }, [])
+    
+    else { setData(null) }
+    // eslint-disable-next-line
+  }, [data1, data2, data3, graph1, graph2])
 
   const series = React.useMemo(
     () => ({
@@ -96,7 +198,11 @@ function RenderScreen() {
 
   return (
     <div className="RenderScreen">
-      <h1 className="PurpleText">Real-Time Bitcoin Graph</h1>
+      <div className="Graph_header">
+        <h1 className="PurpleText">Real-Time Bitcoin Graph</h1>
+        <DropDown options={dropdownOptions} color='#C30F70' displayText='Commodity1' set={setGraph1} />
+        <DropDown options={dropdownOptions} color='#5843BE' displayText='Commodity2' set={setGraph2} />
+      </div>
       <div
         className='Chart_box'
         style={{
@@ -106,7 +212,7 @@ function RenderScreen() {
         }}
       >
         {
-          data1 && series && axes && <Chart data={data1} series={series} axes={axes} tooltip />
+          data && series && axes && <Chart data={data} series={series} axes={axes} tooltip />
         }
       </div>
     </div>
